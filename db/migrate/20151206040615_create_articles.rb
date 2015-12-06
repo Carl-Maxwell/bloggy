0e8883b926d298d453a5c9481531091b874aadb5
Carl Maxwell <carl.maxwell@gmail.com>
2015-12-05 22:04:53 -0800

    create models and controllers

---
+++ db/migrate/20151206040615_create_articles.rb
@@ -0,0 +1,14 @@
+class CreateArticles < ActiveRecord::Migration
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.string :slug
+      t.text :content
+      t.datetime :published_at
+
+      t.timestamps null: false
+    end
+
+    add_index :articles, :slug
+  end
+end
