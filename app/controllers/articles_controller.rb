d0001ee207de944636235a49f4a5a4608ace4fde
Carl Maxwell <carl.maxwell@gmail.com>
2015-12-10 15:18:23 -0800

    * add jqueyr ui datepicker * add jquery UI gem * filled in articles controller actions * add to_param function * add articles new view * add the stylesheet and javascript inclusion stuff back in

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,23 +4,52 @@ class ArticlesController < ApplicationController
     # TODO handle GET[tags] params
     # TODO add pagination
     
+    @articles = Article.all
   end
 
   def show
+    @article = Article.find_by(slug: params[:slug])
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to article_path(@article)
+    else
+
+    end
   end
 
   def edit
+    @article = Article.find_by(slug: params[:slug])
   end
 
   def update
+    @article = Article.find_by(slug: params[:slug])
+
+    if @article.update(article_params)
+      redirect_to article_path(@article)
+    else
+
+    end
   end
 
   def destroy
+    @article = Article.find_by(slug: params[:slug])
+
+    @aricle.delete!
+
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.allow(:article).permit(:title, :slug, :content, :published_at)
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ActiveRecord::Base
+  def to_param
+    slug
+  end
 end
